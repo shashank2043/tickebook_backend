33e7cc9af8986a835476134d974efd1cfd521d5f
Shashank <shashankpulluri1@gmail.com>
2026-02-01 12:23:40 +0530

    Created config server and configured other services to pick configuration from config server

---
+++ config-server/src/test/java/org/team11/tickebook/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team11.tickebook.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
