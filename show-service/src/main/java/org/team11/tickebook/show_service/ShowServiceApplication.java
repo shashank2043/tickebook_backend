f5c82988e837edc394cf6dc1968d930cfce07fe1
Raavi Gopichand <rgopichand04@gmail.com>
2026-02-02 18:21:41 +0530

    small changes

--- show-service/src/main/java/org/team11/tickebook/show_service/ShowServiceApplication.java
+++ show-service/src/main/java/org/team11/tickebook/show_service/ShowServiceApplication.java
@@ -1,9 +1,12 @@
 package org.team11.tickebook.show_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ShowServiceApplication {
 
 	public static void main(String[] args) {
