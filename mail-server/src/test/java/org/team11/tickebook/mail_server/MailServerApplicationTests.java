380e583fc6cf2564a21d2c4c26537f690007c04d
Shashank <shashankpulluri1@gmail.com>
2026-02-03 17:45:54 +0530

    add mail server module with email sending functionality

---
+++ mail-server/src/test/java/org/team11/tickebook/mail_server/MailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team11.tickebook.mail_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
